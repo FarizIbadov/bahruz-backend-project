b19a7b20b56c4beab85487e6cd21921088c3e6cc
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-01-21 19:50:51 +0400

    web:3

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <?php session_start(); ?>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Weather Application By Mehin </title>
+  <title>Weather Application By Bahruz </title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
