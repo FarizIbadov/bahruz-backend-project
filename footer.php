ee60a57b03911605bbe35937bcb11abfe0deca66
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-01-21 19:53:55 +0400

    web:4

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2020 <a href="#">SMehin</a>.</strong>
+    <strong>Copyright &copy; 2014-2020 <a href="#">Bahruz</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
     </div>
